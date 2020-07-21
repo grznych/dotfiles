8a913bfcbf40f1df8acd0d5337e25eab67a6e86a
Elya Grznych <grznych@gmail.com>
2020-07-21 21:34:01 +0500

    change preview and compression settings

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -50,19 +50,19 @@ OPENSCAD_COLORSCHEME=${RNGR_OPENSCAD_COLORSCHEME:-Tomorrow Night}
 handle_extension() {
     case "${FILE_EXTENSION_LOWER}" in
         ## Archive
-        a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
-        rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
-            atool --list -- "${FILE_PATH}" && exit 5
-            bsdtar --list --file "${FILE_PATH}" && exit 5
-            exit 1;;
-        rar)
+       # a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
+       # rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
+       #     atool --list -- "${FILE_PATH}" && exit 5
+       #     bsdtar --list --file "${FILE_PATH}" && exit 5
+       #     exit 1;;
+       # rar)
             ## Avoid password prompt by providing empty password
-            unrar lt -p- -- "${FILE_PATH}" && exit 5
-            exit 1;;
-        7z)
+       #     unrar lt -p- -- "${FILE_PATH}" && exit 5
+       #     exit 1;;
+       # 7z)
             ## Avoid password prompt by providing empty password
-            7z l -p -- "${FILE_PATH}" && exit 5
-            exit 1;;
+       #     7z l -p -- "${FILE_PATH}" && exit 5
+       #     exit 1;;
 
         ## PDF
         pdf)
@@ -133,10 +133,10 @@ handle_image() {
         #     exit 1;;
 
         ## DjVu
-        image/vnd.djvu)
-            ddjvu -format=tiff -quality=90 -page=1 -size="${DEFAULT_SIZE}" \
-                  - "${IMAGE_CACHE_PATH}" < "${FILE_PATH}" \
-                  && exit 6 || exit 1;;
+         image/vnd.djvu)
+             ddjvu -format=tiff -quality=90 -page=1 -size="${DEFAULT_SIZE}" \
+                   - "${IMAGE_CACHE_PATH}" < "${FILE_PATH}" \
+                   && exit 6 || exit 1;;
 
         ## Image
         image/*)
@@ -154,20 +154,20 @@ handle_image() {
             exit 7;;
 
         ## Video
-        video/*)
-            # Thumbnail
-            ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
-            exit 1;;
+         video/*)
+             # Thumbnail
+             ffmpegthumbnailer -i "${FILE_PATH}" -o "${IMAGE_CACHE_PATH}" -s 0 && exit 6
+             exit 1;;
 
         ## PDF
-        application/pdf)
-            pdftoppm -f 1 -l 1 \
-                     -scale-to-x "${DEFAULT_SIZE%x*}" \
-                     -scale-to-y -1 \
-                     -singlefile \
-                     -jpeg -tiffcompression jpeg \
-                     -- "${FILE_PATH}" "${IMAGE_CACHE_PATH%.*}" \
-                && exit 6 || exit 1;;
+         application/pdf)
+             pdftoppm -f 1 -l 1 \
+                      -scale-to-x "${DEFAULT_SIZE%x*}" \
+                      -scale-to-y -1 \
+                      -singlefile \
+                      -jpeg -tiffcompression jpeg \
+                      -- "${FILE_PATH}" "${IMAGE_CACHE_PATH%.*}" \
+                 && exit 6 || exit 1;;
 
 
         ## ePub, MOBI, FB2 (using Calibre)
